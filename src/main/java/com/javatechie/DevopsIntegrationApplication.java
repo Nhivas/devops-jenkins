05f988c15f050059f4d1605e3c5183946d5c2a77
nhivas <nhivas.v2022csbs@sece.ac.in>
2024-10-07 15:46:43 +0530

    final commit 1

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to my first Devops project :)";
+		return "Welcome to my first Devops project :) I am Vengeance";
 	}
 
 	public static void main(String[] args) {
